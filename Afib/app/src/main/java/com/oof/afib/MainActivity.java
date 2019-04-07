a9abea36581fedbc69077bfd9c56f740219172e4
unknown <marianavsgs@gmail.com>
2019-04-07 03:52:27 +0100

    front-end food_screen com clique a funcionar

--- Afib/app/src/main/java/com/oof/afib/MainActivity.java
+++ Afib/app/src/main/java/com/oof/afib/MainActivity.java
@@ -1,5 +1,7 @@
 package com.oof.afib;
 
+import android.content.Intent;
+import android.service.autofill.FillEventHistory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.food_screen);
+        Intent i = new Intent(this, FoodActivity.class);
+        startActivity(i);
     }
 }
