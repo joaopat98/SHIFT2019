ab3179b550ebd241cc92ec0d6d985a08b55ff3a2
João Patrício <joaopat98@gmail.com>
2019-04-07 13:13:36 +0100

    notifs sorta work

--- Afib/app/src/main/java/com/oof/afib/HomepageActivity.java
+++ Afib/app/src/main/java/com/oof/afib/HomepageActivity.java
@@ -5,8 +5,10 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
@@ -20,8 +22,13 @@ import org.json.JSONObject;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class HomepageActivity extends AppCompatActivity {
+
+    static AtomicInteger idGen = new AtomicInteger();
+    public static final String NOTIFICATION_CHANNEL_ID = "10001";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,7 +43,7 @@ public class HomepageActivity extends AppCompatActivity {
                 JSONObject aux = json.getJSONObject(i);
                 Date date = dateFormat.parse((String) aux.get("date"));
                 long unixTime = date.getTime();
-                scheduleNotification(this, unixTime, i);
+                scheduleNotification(unixTime, i);
             }
         }
         catch (Exception e){
@@ -45,7 +52,7 @@ public class HomepageActivity extends AppCompatActivity {
             return;
         }
     }
-    public void scheduleNotification(Context context, long timestamp, int notificationId) {
+    public void scheduleNotification(long timestamp, int notificationId) {
         String CHANNEL_ID = "my_channel_" + Integer.toString(notificationId);
         CharSequence name = CHANNEL_ID;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -53,7 +60,7 @@ public class HomepageActivity extends AppCompatActivity {
             NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
         }
 
-        Notification.Builder builder = new Notification.Builder(context);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
         builder.setContentTitle("Scheduled Notification");
         builder.setContentText("aaaaaaaaaaaaa");
         builder.setSmallIcon(R.drawable.um);
@@ -62,12 +69,39 @@ public class HomepageActivity extends AppCompatActivity {
         }
         Notification notification = builder.build();
 
-        Intent notificationIntent = new Intent(this, MyNotificationPublisher.class);
+        Intent notificationIntent = new Intent(this, NotificationScreenActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(NotificationScreenActivity.class);
+        stackBuilder.addNextIntent(notificationIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(resultPendingIntent);
+
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O)
+        {
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "NOTIFICATION_CHANNEL_NAME", importance);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            assert mNotificationManager != null;
+            builder.setChannelId(NOTIFICATION_CHANNEL_ID);
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+        assert mNotificationManager != null;
+
+        int id = idGen.incrementAndGet();
+        mNotificationManager.notify(id, builder.build());
+
+        /*
         notificationIntent.putExtra(MyNotificationPublisher.NOTIFICATION_ID, notificationId);
         notificationIntent.putExtra(MyNotificationPublisher.NOTIFICATION, notification);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this, notificationId, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         alarmManager.set(AlarmManager.RTC_WAKEUP, timestamp, pendingIntent);
+        */
     }
 }
